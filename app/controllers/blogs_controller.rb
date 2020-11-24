0e3013ef7b98a7aabf616ceeabd1abeb7344d77a
Michael Golenia <mg@codegourmets.io>
2020-11-24 10:14:09 +0100

    refactored before action for portfolio setting

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,6 +2,7 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
   layout "blog"
 
+
   # GET /blogs
   # GET /blogs.json
   def index
