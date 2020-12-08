ab4d9477537b6f29d89268dac830ed22175b07c8
Michael Golenia <mg@codegourmets.io>
2020-12-08 11:31:37 +0100

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
         unless current_user.is_a?(GuestUser) 
             link_to "Logout", destroy_user_session_path, class: style, method: :delete 
         else 
-            (link_to "Register", new_user_registration_path,class: style) +
+            (link_to "Register", new_user_registration_path  ,class: style) +
             (link_to "Login", new_user_session_path, class: style)
         end 
     end
@@ -12,6 +12,45 @@ module ApplicationHelper
         if session[:source]
           content_tag(:p, "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout", class: "source-greeting")
         end
+    end
+
+    def nav_items
+      [
+        {
+          url: root_path, 
+          title: "Home"
+        },
+        {
+          url: about_path, 
+          title: "About Me"
+        },
+        {
+          url: contact_path, 
+          title: "Contact"
+        },
+        {
+          url: blogs_path, 
+          title: "Blog"
+        },
+        {
+          url: portfolios_path, 
+          title: "Portfolio"
+        }
+      ]
+    end
+
+    def nav_helper style, tag_type
+      nav_links = ''
+
+      nav_items.each do |item|
+        nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+      end
+  
+      nav_links.html_safe
+    end
+
+    def active? path
+      "active" if current_page? path
     end
 end
  
